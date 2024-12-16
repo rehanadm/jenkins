12ea8939a5a54910f430d96fe98efe8bc817f255
Abdul Rehan <arehan.techno@gmail.com>
2024-12-16 18:08:24 +0530

    Update check-container.sh

--- check-container.sh
+++ check-container.sh
@@ -1,10 +1,10 @@
-##!/bin/bash
+#!/bin/bash
 # Get the list of container IDs
-#CONTAINERS=$(docker ps -a -q)
+CONTAINERS=$(docker ps -a -q)
 
 # Check if there are any containers to remove
-#if [ -n "$CONTAINERS" ]; then
-#sudo docker rm -f $CONTAINERS
-#else
-#  echo "No containers to remove"
-#fi
+if [ -n "$CONTAINERS" ]; then
+docker rm -f $CONTAINERS
+else
+ echo "No containers to remove"
+fi
